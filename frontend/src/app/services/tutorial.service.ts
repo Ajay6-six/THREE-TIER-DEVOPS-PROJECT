1561b174db799e17d4fe70a364d9ed60994ccf5b
10msnarun <108964134+10msnarun@users.noreply.github.com>
2025-12-11 23:41:35 +0530

    4th commit

--- frontend/src/app/services/tutorial.service.ts
+++ frontend/src/app/services/tutorial.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Tutorial } from '../models/tutorial.model';
 
-const baseUrl = 'http://localhost:8080/api/tutorials';
+const baseUrl = 'http://3.110.54.235:8080/api/tutorials';
 
 @Injectable({
   providedIn: 'root'
